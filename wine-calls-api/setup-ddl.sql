919adee90578604c1b2601e622c5bd3614a5f930
mattvasc <mattvasc@outlook.com>
2018-10-14 22:49:02 -0300

    adicionando sql

--- wine-calls-api/setup-ddl.sql
+++ wine-calls-api/setup-ddl.sql
@@ -1,61 +1,121 @@
+CREATE DATABASE  IF NOT EXISTS `wine_calls` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `wine_calls`;
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: wine_calls
+-- ------------------------------------------------------
+-- Server version	5.7.23-0ubuntu0.18.04.1
 
--- MySQL Workbench Forward Engineering
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+--
+-- Table structure for table `client`
+--
 
--- -----------------------------------------------------
--- Schema wine_chamados
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema wine_chamados
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `wine_chamados` DEFAULT CHARACTER SET utf8 ;
-USE `wine_chamados` ;
-
--- -----------------------------------------------------
--- Table `wine_chamados`.`client`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `wine_chamados`.`client` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(64) NOT NULL,
-  `email` VARCHAR(128) NULL,
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `email` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `company`
+--
 
--- -----------------------------------------------------
--- Table `wine_chamados`.`company`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `wine_chamados`.`company` (
-  `cnpj` VARCHAR(14) NOT NULL,
-  `name` VARCHAR(64) NULL,
-  `address` VARCHAR(128) NULL,
-  `site` VARCHAR(64) NULL,
-  `email` VARCHAR(128) NULL,
-  `phone` VARCHAR(128) NULL,
-  PRIMARY KEY (`cnpj`))
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `cnpj` varchar(14) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `address` varchar(128) DEFAULT NULL,
+  `site` varchar(64) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `phone` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`cnpj`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `technician`
+--
 
--- -----------------------------------------------------
--- Table `wine_chamados`.`technician`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `wine_chamados`.`technician` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `address` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `technician`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `technician` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `ticket`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ticket` (
+  `ticket_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) DEFAULT NULL,
+  `technician_id` int(11) DEFAULT NULL,
+  `client_ticket_id` varchar(45) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  `client_price` float DEFAULT NULL,
+  `technician_price` float DEFAULT NULL,
+  `ticket_status_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ticket_id`),
+  UNIQUE KEY `call_id_UNIQUE` (`ticket_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ticket_status`
+--
+
+DROP TABLE IF EXISTS `ticket_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ticket_status` (
+  `ticket_status_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(45) NOT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`ticket_status_id`),
+  UNIQUE KEY `ticket_status_id_UNIQUE` (`ticket_status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-14 22:46:47
 
 CREATE USER IF NOT EXISTS 'wine_dbadmin'@'%' IDENTIFIED BY 'wine_DS35uqgTFa';
 

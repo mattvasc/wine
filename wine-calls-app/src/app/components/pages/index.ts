235ef021f25734a6a4fc7c499a5696c802260dc6
Bruno Donato Banhos <bruno.donato.banhos@gmail.com>
2018-10-20 20:15:47 -0300

    Unificação de imports. Apenas organização.

---
+++ wine-calls-app/src/app/components/pages/index.ts
@@ -0,0 +1,5 @@
+export { NotFoundComponent } from './not-found/not-found.component';
+export { TechnicianCreateUpdateComponent } from './technician-create-update/technician-create-update.component';
+export { TechnicianListComponent } from './technician-list/technician-list.component';
+export { TechnicianMainComponent } from './technician-main/technician-main.component';
+export { TicketMainComponent } from './ticket-main/ticket-main.component';

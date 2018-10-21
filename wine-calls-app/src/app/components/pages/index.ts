9b4dc9dc82b8da905727c3f2358465dcb2c1c679
Bruno Donato Banhos <bruno.donato.banhos@gmail.com>
2018-10-20 21:10:09 -0300

    Movendo mais alguns arquivos para components/pages
    Em app.module.ts preciso encontrar uma forma mais elegante de pegar todos os imports de pages.

--- wine-calls-app/src/app/components/pages/index.ts
+++ wine-calls-app/src/app/components/pages/index.ts
@@ -1,3 +1,7 @@
+export { ClientCreateUpdateComponent } from './client-create-update/client-create-update.component';
+export { ClientListComponent } from './client-list/client-list.component';
+export { ClientMainComponent } from './client-main/client-main.component';
+export { MainComponent } from './main/main.component'; 
 export { NotFoundComponent } from './not-found/not-found.component';
 export { TechnicianCreateUpdateComponent } from './technician-create-update/technician-create-update.component';
 export { TechnicianListComponent } from './technician-list/technician-list.component';

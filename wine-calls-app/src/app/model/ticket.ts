cb9b5abd2e70733e516df53a9c152e755938a6ba
Evelin Soares <t-evsoar@microsoft.com>
2018-10-31 23:10:57 -0300

    progress

--- wine-calls-app/src/app/model/ticket.ts
+++ wine-calls-app/src/app/model/ticket.ts
@@ -8,9 +8,13 @@ export class Ticket{
 	/* Fim fks */
 	public ticket_client_id ?: string; // nao eh fk
 	public data_criacao ?: string;
-	public data_agendamento ?: string;
 	public preco_cliente ?: number;
 	public preco_tecnico ?: number;
+	public ticket_status_id ?: string;
+	public data_agendamento ?: string;
+	public horario_agendamento ?: string;
+
+
 	public situacao ?: string;
 	public observacoes?: string;
 }

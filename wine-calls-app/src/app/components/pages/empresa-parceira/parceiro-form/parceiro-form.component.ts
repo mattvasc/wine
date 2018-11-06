9043276611c02d049df5d06fd71417ad0d2add4a
mattvasc <mattvasc@outlook.com>
2018-11-05 23:53:27 -0200

    bugfix no parceiro

--- wine-calls-app/src/app/components/pages/empresa-parceira/parceiro-form/parceiro-form.component.ts
+++ wine-calls-app/src/app/components/pages/empresa-parceira/parceiro-form/parceiro-form.component.ts
@@ -24,59 +24,59 @@ export class ParceiroFormComponent implements OnInit {
   constructor(private api: EmpresaParceiraService,
   private router: Router,
 private route: ActivatedRoute) {
-    this.parceiroAtual = new EmpresaParceira();
-    this.pagamentoAtual = new Pagamento();
-    this.parceiroAtual.endereco = new Endereco();
-    console.log("**********");
-    console.log(this.parceiroAtual);
-    console.log("**********");
+  
+  this.isSalvar = true;
+}
 
-    this.isSalvar = true;
-   }
+ngOnInit() {
+  this.parceiroAtual = new EmpresaParceira();
+  this.parceiroAtual.endereco = new Endereco();
+  this.pagamentoAtual = new Pagamento();
+  this.isPJ = 'juridica';
+  
+  this.route.params.subscribe(params => {
+    this.id = params['id'];
+    // se o usuário acessou a pagina sem passar um id inteiro...
+    if (this.id !== undefined && isNaN(this.id)) {
+      // TODO: Proteger a página, saindo dela quando informar id invalido
+      alert("Acessando a pagina de maneira inválida!");
+      this.router.navigateByUrl('/');
+    }
+    // Se for um Update
+    if (this.id !== undefined) {
+      this.isSalvar = false;
+      this.id = + this.id; // Cast string to int
+      console.log("Indo buscar o id tal" + this.id);
+      this.api.getSingle(this.id).subscribe(ret => {
+        console.log(ret);
+        if (ret !== null) {
+          // TODO: Verificar o success
+          this.parceiroAtual = ret["data"]["empresa_parceira"];
+          
+        } else {
+          // TODO: Proteger a página, saindo dela quando informar id invalido
+          alert("Acessando a pagina com um id inválido!");
+          this.router.navigateByUrl('/');
+        }
+      });
+      
+    }
+    
+  });
+  
+  console.log("**********");
+  console.log(this.parceiroAtual);
+  console.log("**********");
+}
 
-   ngOnInit() {
-     this.isPJ = 'pessoaJuridica';
-
-     this.route.params.subscribe(params => {
-       this.id = params['id'];
-       // se o usuário acessou a pagina sem passar um id inteiro...
-       if (this.id !== undefined && isNaN(this.id)) {
-         // TODO: Proteger a página, saindo dela quando informar id invalido
-         alert("Acessando a pagina de maneira inválida!");
-         this.router.navigateByUrl('/');
-       }
-       // Se for um Update
-       if (this.id !== undefined) {
-         this.isSalvar = false;
-         this.id = + this.id; // Cast string to int
-       console.log("Indo buscar o id tal" + this.id);
-         this.api.getSingle(this.id).subscribe(ret => {
-           console.log(ret);
-           if (ret !== null) {
-             // TODO: Verificar o success
-             this.parceiroAtual = ret["data"]["empresa_parceira"];
-
-           } else {
-             // TODO: Proteger a página, saindo dela quando informar id invalido
-             alert("Acessando a pagina com um id inválido!");
-             this.router.navigateByUrl('/');
-           }
-         });
-
-       }
-
-     });
-
-   }
-
-   salvar() {
-     // TODO: ARRUMAR ISSO AI
-     delete this.parceiroAtual.data_rg;
-     // TODO: Verificar dados
-     this.parceiroAtual.pagamento = this.pagamentoAtual;
-     this.api.create(this.parceiroAtual).subscribe(retorno => {
-       console.log(retorno);
-       if(retorno !== undefined && retorno['success'] === true)
+salvar() {
+  // TODO: ARRUMAR ISSO AI
+  delete this.parceiroAtual.data_rg;
+  // TODO: Verificar dados
+  this.parceiroAtual.pagamento = this.pagamentoAtual;
+  this.api.create(this.parceiroAtual).subscribe(retorno => {
+    console.log(retorno);
+    if(retorno !== undefined && retorno['success'] === true)
         {
           alert("Parceiro Salvo com Sucesso!");
         }
--- wine-calls-app/src/app/masks.ts
+++ wine-calls-app/src/app/masks.ts
@@ -2,5 +2,6 @@ export class Masks {
   public static CEP = [ /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/];
   public static CNPJ = [/\d/,/\d/,'\.',/\d/,/\d/,/\d/,'\.', /\d/,/\d/,/\d/,'\/',/\d/,/\d/,/\d/,/\d/,'-',/\d/,/\d/,];
   public static CPF = [/\d/,/\d/,/\d/,'\.',/\d/,/\d/,/\d/,'\.',/\d/,/\d/,/\d/,'-',/\d/,/\d/];
+public static CPF2 = [/\d/,/\d/,/\d/,'\.',/\d/,/\d/,/\d/,'\.',/\d/,/\d/,/\d/,'-',/\d/,/\d/];
   public static DATE = [/\d/,/\d/,'\/',/\d/,/\d/,'\/',/\d/,/\d/,/\d/,/\d/,];
 }

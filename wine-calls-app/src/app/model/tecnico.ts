270596c92c23270c74fce163585fcf97dd842640
evelinsoares <evelincontatos@hotmail.com>
2018-11-25 13:39:29 -0200

    tecnico: model app/appi e form OK. Falta instanciar objeto

--- wine-calls-app/src/app/model/tecnico.ts
+++ wine-calls-app/src/app/model/tecnico.ts
@@ -1,12 +1,15 @@
 export class Tecnico {
     public id?: number;
+    public status?: string;
     public nome?: string;
     public email?: string;
     public telefones?: string;
     public cpf?: string;
     public rg?: string;
     public data_rg?: string;
+    public nome_pai?: string;
+    public nome_mae?: string;
+    public nascimento?: string;
     public empresa_do_tecnico_id?: number;
-    public status?: string;
     public observacoes?: string;
 }

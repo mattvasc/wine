338244c29c8fd8bac611979c363cae0c50709b14
mattvasc <mattvasc@outlook.com>
2018-12-09 16:40:52 -0200

    Agora a rota de email envia 1 email ou N emails

--- wine-calls-api/credentials.js
+++ wine-calls-api/credentials.js
@@ -7,7 +7,7 @@ user: process.env.DB_USER,
 password: process.env.DB_PASS,
 database: process.env.DB,
 db_type: process.env.DB_TYPE,
-sparkpost: "4f398b333ff7e6047a25cb867506ebd7abc5d2c0"
+sparkpost: process.env.SPARKPOST
 };
 credentials.connection_string = `${credentials.db_type}://${credentials.host}:${credentials.port}/${credentials.database}`
 
--- wine-calls-api/routes/Email.js
+++ wine-calls-api/routes/Email.js
@@ -3,26 +3,42 @@ var router = express.Router();
 var credentials = require('../credentials.js')
 const SparkPost = require('sparkpost');
 
-router.post('/enviar', function (req, res) {
-  const client = new SparkPost(credentials['sparkpost']);
 
+router.post('/enviar', function (req, res) {
+
+  const client = new SparkPost(credentials['sparkpost']);
+  let temp;
+  try {
+    temp = JSON.parse(req.body.para);
+  } catch(e) {
+    temp = req.body.para;
+  }
+  let enviar = [];
+  if(Array.isArray(temp)) {
+    for(let i = 0; i < temp.length; i++)
+      enviar.push({address: temp[i]});
+  } else
+    enviar.push({address: temp});
+    
   client.transmissions.send({
     content: {
-      from: 'buzzo@neeko.com.br',
+      from: process.env.SENDER,
       subject: req.body.titulo,
       html: req.body.texto
     },
-    recipients: [
-      {address: req.body.para}
-    ]
+    recipients: enviar
   })
   .then(data => {
     console.log('E-mail enviado com sucesso');
     console.log(data);
+    res.statusCode = 200;
+    res.json({success:true, data: 'E-mail enviado com sucesso'});
   })
   .catch(err => {
     console.log('Envio de e-mail falhou');
     console.log(err);
+    res.statusCode = 500;
+    res.json({success:false, data: 'Falha ao enviar email!'});
   });
 });
 
